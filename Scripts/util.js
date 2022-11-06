0b1c20f1186e5f21cef45586912b737d254c825a
Jesus Osorio <jaoz10.16@gmail.com>
2022-11-06 01:34:47 -0500

    Add prettier format

--- Scripts/util.js
+++ Scripts/util.js
@@ -12,13 +12,16 @@ const calcularHora = (a, b) => {
 };
 
 // Funcion para seleccionar la hora mayor
-const mayorHora = (a, b , hora) => {
+const mayorHora = (a, b, hora) => {
   if (parseInt(a) > parseInt(b.resultado)) {
     b = {
       resultado: a,
       hora: hora.slice(6, 11),
     };
-  } else if ((parseInt(a) == parseInt(b.resultado) && (parseInt((a.split(':')[1])) > parseInt(b.resultado.split(':')[1])))) {
+  } else if (
+    parseInt(a) == parseInt(b.resultado) &&
+    parseInt(a.split(":")[1]) > parseInt(b.resultado.split(":")[1])
+  ) {
     b = {
       resultado: a,
       hora: hora.slice(6, 11),
@@ -32,33 +35,41 @@ export default function resolverCaso(caso) {
 
   const db = []
   for (let i = 0; i < caso.length; i++) {
-    let dia = caso[i].split(',').sort()
-  
+    let dia = caso[i].split(",").sort();
+
     // se compara compara con la primera diferencia horario
     let res = {
-    resultado: calcularHora(dia[0].slice(0, 5), "08:00"),
-    hora: dia[0].slice(0, 5),
+      resultado: calcularHora(dia[0].slice(0, 5), "08:00"),
+      hora: '08:00',
     };
     let final = calcularHora("50:00", dia[dia.length - 1].slice(6, 11));
-  
+
     // se evalua con la ultima hora laboral
-    res = mayorHora(final, res, dia[dia.length-1])
-  
+    res = mayorHora(final, res, dia[dia.length - 1]);
+
     // se evaluan todas las horas
-    for (let i = 0; i < dia.length; i++) {
-    let opc = "";
-    try {
+    for (let i = 0; i < dia.length - 1; i++) {
+      let opc = "";
+      try {
         opc = calcularHora(dia[i + 1].slice(0, 5), dia[i].slice(6, 11));
-    } catch (e) {
+      } catch (e) {
         opc = "0";
-    }
-    res = mayorHora(opc, res, dia[i])
+      }
+      res = mayorHora(opc, res, dia[i]);
     }
 
-    if(parseInt(res.resultado.split(':')[0]) == 0){
-      db.push(`Día #${i+1}: La siesta es a las ${res.hora} y será de ${res.resultado.split(':')[1]} minutos \n\n`);
-    }else{
-      db.push(`Día #${i+1}: La siesta es a las ${res.hora} y será de ${parseInt(res.resultado.split(':')[0])} horas y ${res.resultado.split(':')[1]} minutos \n\n`);
+    if (parseInt(res.resultado.split(":")[0]) == 0) {
+      db.push(
+        `Día #${i + 1}: La siesta es a las ${res.hora} y será de ${
+          res.resultado.split(":")[1]
+        } minutos \n\n`
+      );
+    } else {
+      db.push(
+        `Día #${i + 1}: La siesta es a las ${res.hora} y será de ${parseInt(
+          res.resultado.split(":")[0]
+        )} horas y ${res.resultado.split(":")[1]} minutos \n\n`
+      );
     }
   }
   return db; 
